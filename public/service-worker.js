354b88e2b6b129aa43f1081e3459603db61e7999
notjustprincexx <emekaprince224@gmail.com>
2026-03-11 19:44:52 +0100

    update

---
+++ public/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "medexcel-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/offline.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.mode === "navigate") {
+
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match("/offline.html");
+      })
+    );
+
+  }
+
+});
